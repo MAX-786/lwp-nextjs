2fb27787b29e5afd0594bf26926e3112e2176474
MAX-786 <hussainmohammad328@gmail.com>
2025-01-25 21:38:43 +0530

    configuring babel to fix es6 errors

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,18 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+module.exports = createJestConfig({
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    '^@/context/(.*)$': '<rootDir>/context/$1',
-    '\\.(css|scss)$': 'identity-obj-proxy'
+    "^@/context/(.*)$": "<rootDir>/src/context/$1",
+    "\\.(css|scss)$": "identity-obj-proxy",
   },
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/']
-};
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+});
